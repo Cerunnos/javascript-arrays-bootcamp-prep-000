936fc6c13d2b5a2886aeeb46d47128da11bba27e
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-11-05 17:47:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,5 +14,5 @@ function destructivelyAddElementToEndOfArray(array, element){
   return array
 }
 function accessElementInArray(array, index){
-  return console.log(array[index])
+  return console.log(`${array[index]})
 }
