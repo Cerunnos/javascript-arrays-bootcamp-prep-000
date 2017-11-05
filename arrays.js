3026e00bb9cb6bd90b16bc86bc6b579c089f1ef1
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-11-05 17:39:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,5 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return [...array, element]
+  return array(array.unshift(element))
 }
